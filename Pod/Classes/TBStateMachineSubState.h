3418db6d23c3f79ec4a31d64deedcd65073f9060
Julian Krumow <julian.krumow@tarbrain.com>
2014-09-19 20:00:54 +0200

    TBStateMachineSubState inherits from TBStateMachineState.

--- Pod/Classes/TBStateMachineSubState.h
+++ Pod/Classes/TBStateMachineSubState.h
@@ -11,4 +11,27 @@
 
 @interface TBStateMachineSubState : TBStateMachineState
 
+/**
+ *  Creates a `TBStateMachineSubState` instance from a given name.
+ *
+ *  Throws a `TBStateMachineException` when name is nil or an empty string.
+ *
+ *  @param name The specified sub state name.
+ *
+ *  @return The sub state instance.
+ */
++ (TBStateMachineSubState *)subStateWithName:(NSString *)name stateMachine:(TBStateMachine *)stateMachine;
+
+/**
+ *  Initializes a `TBStateMachineParallelState` with a specified name.
+ *
+ *  Throws a `TBStateMachineException` when name is nil or an empty string.
+ *
+ *  @param name The name of this wrapper. Must be unique.
+ *
+ *  @return An initialized `TBStateMachineParallelState` instance.
+ */
+- (instancetype)initWithName:(NSString *)name stateMachine:(TBStateMachine *)stateMachine;
+
+
 @end
--- Pod/Classes/TBStateMachineSubState.m
+++ Pod/Classes/TBStateMachineSubState.m
@@ -7,7 +7,29 @@
 //
 
 #import "TBStateMachineSubState.h"
+#import "NSException+TBStateMachine.h"
+
+@interface TBStateMachineSubState ()
+
+@property (nonatomic, strong) TBStateMachine *stateMachine;
+
+@end
 
 @implementation TBStateMachineSubState
+
+
++ (TBStateMachineSubState *)subStateWithName:(NSString *)name stateMachine:(TBStateMachine *)stateMachine
+{
+    return [[TBStateMachineSubState alloc] initWithName:name stateMachine:stateMachine];
+}
+
+- (instancetype)initWithName:(NSString *)name stateMachine:(TBStateMachine *)stateMachine
+{
+    self = [super initWithName:name];
+    if (self) {
+        _stateMachine = stateMachine;
+    }
+    return self;
+}
 
 @end
