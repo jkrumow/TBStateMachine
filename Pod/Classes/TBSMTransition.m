51bc8b8bc33aaf677cb9de6f82219de031fd975c
Julian Krumow <julian.krumow@tarbrain.com>
2015-02-07 01:52:48 +0100

    Added safeguard.

--- Pod/Classes/TBSMTransition.m
+++ Pod/Classes/TBSMTransition.m
@@ -57,6 +57,8 @@
         if ([self.sourceState.path containsObject:self.targetState] || [self.targetState.path containsObject:self.sourceState]) {
             TBSMSubState *containingSubState = (TBSMSubState *)lca.currentState;
             lca = containingSubState.stateMachine;
+        } else {
+            lca = nil;
         }
     }
     if (!lca) {
