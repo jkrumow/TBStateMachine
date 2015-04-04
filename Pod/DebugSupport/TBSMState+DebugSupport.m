7e4ab1ac37845c428bd95023bd69d7f0cc1fa4df
Julian Krumow <julian.krumow@tarbrain.com>
2015-04-04 22:24:28 +0200

    Remove unnecessary swizzle-code.

--- Pod/DebugSupport/TBSMState+DebugSupport.m
+++ Pod/DebugSupport/TBSMState+DebugSupport.m
@@ -24,20 +24,12 @@
         Method originalMethod = class_getInstanceMethod(class, originalSelector);
         Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
         
-        BOOL didAddMethod =
         class_addMethod(class,
                         originalSelector,
                         method_getImplementation(swizzledMethod),
                         method_getTypeEncoding(swizzledMethod));
         
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+        method_exchangeImplementations(originalMethod, swizzledMethod);
         
         originalSelector = @selector(exit:targetState:data:);
         swizzledSelector = @selector(tb_exit:targetState:data:);
@@ -45,21 +37,12 @@
         originalMethod = class_getInstanceMethod(class, originalSelector);
         swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
         
-        didAddMethod =
         class_addMethod(class,
                         originalSelector,
                         method_getImplementation(swizzledMethod),
                         method_getTypeEncoding(swizzledMethod));
         
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-        
+        method_exchangeImplementations(originalMethod, swizzledMethod);
     });
 }
 
--- Pod/DebugSupport/TBSMStateMachine+DebugSupport.m
+++ Pod/DebugSupport/TBSMStateMachine+DebugSupport.m
@@ -62,20 +62,15 @@ NSString * const TBSMDebugSupportException = @"TBSMDebugSupportException";
         Method originalMethod = class_getInstanceMethod(class, originalSelector);
         Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
         
-        BOOL didAddMethod =
         class_addMethod(class,
                         originalSelector,
                         method_getImplementation(swizzledMethod),
                         method_getTypeEncoding(swizzledMethod));
         
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+        class_replaceMethod(class,
+                            swizzledSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
         
         class = [TBSMStateMachine class];
         
@@ -86,20 +81,12 @@ NSString * const TBSMDebugSupportException = @"TBSMDebugSupportException";
         originalMethod = class_getInstanceMethod(class, originalSelector);
         swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
         
-        didAddMethod =
         class_addMethod(class,
                         originalSelector,
                         method_getImplementation(swizzledMethod),
                         method_getTypeEncoding(swizzledMethod));
         
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+        method_exchangeImplementations(originalMethod, swizzledMethod);
         
         // -tearDown:
         originalSelector = @selector(tearDown:);
@@ -108,20 +95,12 @@ NSString * const TBSMDebugSupportException = @"TBSMDebugSupportException";
         originalMethod = class_getInstanceMethod(class, originalSelector);
         swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
         
-        didAddMethod =
         class_addMethod(class,
                         originalSelector,
                         method_getImplementation(swizzledMethod),
                         method_getTypeEncoding(swizzledMethod));
         
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+        method_exchangeImplementations(originalMethod, swizzledMethod);
     });
 }
 
