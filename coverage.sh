ba7b26a1c7781f5e9115abab595f33a3b2783ff5
Julian Krumow <julian.krumow@bogusmachine.com>
2017-05-31 00:16:55 +0200

    Revert "Add slather to project."
    This reverts commit ebc65beb726c576d14e90dd2f9a88d2b51ab6ee8.

--- coverage.sh
+++
@@ -1,4 +0,0 @@
-cd Example
-bundle exec slather coverage --html --output-directory ../reports/coverage/ --ignore "Tests/"\* --ignore Pods/\* --scheme TBStateMachineTests --workspace TBStateMachine.xcworkspace/ TBStateMachine.xcodeproj/
-cd ..
-open reports/coverage/index.html
